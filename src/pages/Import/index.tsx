5d8e90a6f971f82087bfd8dc93456a292fbe7aea
Carlos Gabriel <gabrielantero2012@hotmail.com>
2020-04-20 22:44:36 -0300

    all tests checked

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -23,19 +23,31 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
+    const data = new FormData();
 
     try {
-      // await api.post('/transactions/import', data);
+      /* eslint-disable */
+      for (const uploadedFile of uploadedFiles) {
+        data.set('file', uploadedFile.file, uploadedFile.name);
+
+        await api.post('transactions/import', data);
+      }
+       /* eslint-disable */
+
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const setFiles = files.map((file) => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles([...uploadedFiles, ...setFiles]);
   }
 
   return (
